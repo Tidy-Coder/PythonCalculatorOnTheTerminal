9e2a0f00feaef71fd9b94548c0790ba5f9cce340
TidyCodeur <157531579+TidyCodeur@users.noreply.github.com>
2024-01-25 13:07:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def checkCharsAndSyntax(contentToCheck, toCheckChars = None, toCheckWords = None
       localistaionContent = 0
       for i in contentToCheck:
         if z[0] == i:
-          if contentToCheck[localistaionContent:len(toCheckWords)+1] == toCheckWords and CheckIfNoReservedPlaceHere((localistaionContent, len(toCheckWords)+1), reservedplaces):
+          if contentToCheck[localistaionContent:len(toCheckWords)+1] == z and CheckIfNoReservedPlaceHere((localistaionContent, len(toCheckWords)+1), reservedplaces):
             reservedplaces += (localistaionContent, len(toCheckWords)+1)
             isOkay += len(toCheckWords)
             print("haha")
