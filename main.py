80e7529902a82aaa09f91d3f28c241b63f72baa4
TidyCodeur <157531579+TidyCodeur@users.noreply.github.com>
2024-01-25 12:59:51 +0100

    Update main.py
    A update for main.py

--- main.py
+++ main.py
@@ -94,4 +94,4 @@ while entrance == "" or not checkCharsAndSyntax(entrance, allValid[:-1], allVali
   entrance = input()
 
 # After Check, doing the operation with eval function...
-print(entrance + " = " + str(eval(entrance.replace("modulo", "%"))))
+print(entrance + " = " + str(eval(entrance.replace("modulo", "%").replace(" ", ""))))
